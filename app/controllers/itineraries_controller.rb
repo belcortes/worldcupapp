b58b9331252141f2fe1b1128c09f10d94b0b2087
alexdavidow <alex.davidow@gmail.com>
2013-06-13 19:35:09 -0400

    added game listings to city show pages, fixed route to itinerary on layout, built itinerary controller

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,23 +1,45 @@
 class ItinerariesController < ApplicationController
 
   def index
+    @itineraries = Itinerary.all
+    if current_user.itineraries >= 1
+    else
+      redirect_to new_itinerary_path
+    end
   end
 
   def new
+    @itinerary = Itinerary.new
   end
 
   def create
+    @itinerary = Itinerary.new(params[:itinerary])
+    if @itinerary.save
+      redirect_to itineraries_path
+    else
+      render :new
   end
 
   def show
+    @itinerary = Itinerary.find(params[:id])
   end
 
   def edit
+    @itinerary = Itinerary.find(params[:id])
   end
 
   def update
+    itinerary = Itinerary.find(params[:id])
+    if itinerary.update_attributes(params[:itinerary])
+      redirect_to itineraries_path
+    else
+      redirect_to edit_itinerary_path
+    end
   end
 
   def destroy
+    itinerary = Itinerary.find(params[:id])
+    itinerary.destroy
+    redirect_to :back
   end
 end
