532f734081440b3f45a14f76e857386027b8da84
Isabel Cortes <belcortes@gmail.com>
2013-06-13 23:00:07 -0400

    finished admin priviledges and user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      render :nothing => true
+      redirect_to '/'
     else
       render :new
     end
